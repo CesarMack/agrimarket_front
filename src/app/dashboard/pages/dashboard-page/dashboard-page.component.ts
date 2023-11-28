1b768ae8185d86f1f10d9fe036b6f529a891a028
Cesar Mackintosh <cesar@cilabs.io>
2023-11-28 15:10:15 -0800

    chart added

--- src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dashboard } from '../../interfaces/dashboard';
 import { FarmerService } from '../../services/farmer.service';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -8,6 +9,8 @@ import { FarmerService } from '../../services/farmer.service';
   styleUrls: ['./dashboard-page.component.css'],
 })
 export class DashboardPageComponent implements OnInit {
+  public chart!: Chart;
+
   dashboardData: Dashboard | undefined;
   constructor(private farmerService: FarmerService) {}
   ngOnInit(): void {
@@ -19,5 +22,23 @@ export class DashboardPageComponent implements OnInit {
         console.error('Error fetching dashboard data:', error);
       }
     );
+
+    const labels = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"];
+    const data = {
+      labels: labels,
+      datasets: [{
+        backgroundColor: "#4c51bf",
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: "#4c51bf",
+        tension: 0.1
+      }]
+    };
+
+    this.chart = new Chart("chart", {
+      type: 'line',
+      data
+    })
   }
 }
