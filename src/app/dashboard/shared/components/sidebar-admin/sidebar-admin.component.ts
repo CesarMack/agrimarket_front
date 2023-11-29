9eb2ad94f13d5e45343ba67501bc18b364ba20c7
Cesar Mackintosh <cesar@cilabs.io>
2023-11-28 23:16:23 -0800

    farmer sidebar

--- src/app/dashboard/shared/components/sidebar-admin/sidebar-admin.component.ts
+++ src/app/dashboard/shared/components/sidebar-admin/sidebar-admin.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-sidebar-admin',
   templateUrl: './sidebar-admin.component.html',
 })
 export class SidebarAdminComponent {
+  isList!: number;
+    isMenu: boolean = false;
+    isSearch: boolean = false;
   collapseShow = 'hidden';
   constructor() {}
 
@@ -13,3 +16,4 @@ export class SidebarAdminComponent {
     this.collapseShow = classes;
   }
 }
+
