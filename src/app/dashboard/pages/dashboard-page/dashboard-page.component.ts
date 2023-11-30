605eec6446ceb8cd729c3d51a38b012e58902e94
Cesar Mackintosh <cesar@cilabs.io>
2023-11-30 04:32:54 -0800

    dashbord farmer

--- src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -10,9 +10,15 @@ import { Chart } from 'chart.js/auto';
 export class DashboardPageComponent implements OnInit {
   public chart!: Chart;
   public secondChart!: Chart;
+  public completeOrders!: String;
+  public canceledOrders!: String;
+  public activeProducts!: String;
+  public pendingOrders!: String;
 
   dashboardData: Dashboard | undefined;
+
   constructor(private farmerService: FarmerService) {}
+
   ngOnInit(): void {
     this.farmerService.getDashboard().subscribe(
       (data) => {
@@ -23,18 +29,18 @@ export class DashboardPageComponent implements OnInit {
       }
     );
 
-    /*First */
+    // Inicializar gráficas con datos predeterminados
+    this.initializeCharts();
 
-    const labels = [
-      'Enero',
-      'Febrero',
-      'Marzo',
-      'Abril',
-      'Mayo',
-      'Junio',
-      'Julio',
-    ];
-    const data = {
+    this.activeProducts = "0";
+    this.completeOrders = "0";
+    this.pendingOrders = "0";
+    this.canceledOrders = "0";
+  }
+
+  initializeCharts() {
+    const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+    const initialData = {
       labels: labels,
       datasets: [
         {
@@ -47,11 +53,91 @@ export class DashboardPageComponent implements OnInit {
         },
       ],
     };
-    /*Second */
 
-    this.secondChart = new Chart('chart', {
+    // Inicializar gráficas con datos predeterminados
+    this.chart = new Chart('chart', {
       type: 'line',
-      data,
+      data: initialData,
     });
+
+    this.secondChart = new Chart('secondChart', {
+      type: 'line',
+      data: initialData,
+    });
+  }
+
+  updateChartData(newData: any) {
+    // Actualizar data de gráficas y volver a renderizar
+    this.chart.data = newData;
+    this.chart.update();
+
+    this.secondChart.data = newData;
+    this.secondChart.update();
+  }
+
+  weekFilter() {
+    // Lógica de filtrado por semana
+    const newData = {
+      labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4'],
+      datasets: [
+        {
+          backgroundColor: '#4c51bf',
+          label: 'My First Dataset',
+          data: [30, 45, 60, 75],
+          fill: false,
+          borderColor: '#4c51bf',
+          tension: 0.1,
+        },
+      ],
+    };
+
+    this.updateChartData(newData);
+    this.completeOrders = "1";
+    this.pendingOrders = "1";
+    this.canceledOrders = "1";
+  }
+
+  monthFilter() {
+    // Lógica de filtrado por mes
+    const newData = {
+      labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
+      datasets: [
+        {
+          backgroundColor: '#4c51bf',
+          label: 'My First Dataset',
+          data: [50, 65, 80, 95, 110],
+          fill: false,
+          borderColor: '#4c51bf',
+          tension: 0.1,
+        },
+      ],
+    };
+
+    this.updateChartData(newData);
+    this.completeOrders = "2";
+    this.pendingOrders =  "2";
+    this.canceledOrders = "2";
+  }
+
+  yearFilter() {
+    // Lógica de filtrado por año
+    const newData = {
+      labels: ['2021', '2022', '2023'],
+      datasets: [
+        {
+          backgroundColor: '#4c51bf',
+          label: 'My First Dataset',
+          data: [100, 120, 140],
+          fill: false,
+          borderColor: '#4c51bf',
+          tension: 0.1,
+        },
+      ],
+    };
+
+    this.updateChartData(newData);
+    this.completeOrders = "3";
+    this.pendingOrders = "3";
+    this.canceledOrders = "3";
   }
 }
--- src/app/dashboard/shared/components/header-stats/header-stats.component.ts
+++ src/app/dashboard/shared/components/header-stats/header-stats.component.ts
@@ -1,7 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'shared-header-stats',
   templateUrl: './header-stats.component.html',
 })
-export class HeaderStatsComponent {}
+export class HeaderStatsComponent implements OnInit {
+  title!: string;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = this.router.url;
+        this.updateTitleBasedOnRoute(currentRoute);
+      }
+    });
+    const currentRoute = this.router.url;
+    this.updateTitleBasedOnRoute(currentRoute);
+  }
+
+  private updateTitleBasedOnRoute(route: string): void {
+    if (route === '/farmer') {
+      this.title = 'Dashboard';
+    } else if (route === '/farmer/orders') {
+      this.title = 'Órdenes';
+    } else if (route === '/farmer/orders/completed') {
+      this.title = 'Órdenes';
+    } else if (route === '/farmer/orders/pending') {
+      this.title = 'Órdenes';
+    } else if (route === '/farmer/orders/canceled') {
+      this.title = 'Órdenes';
+    } else if (route === '/farmer/products') {
+      this.title = 'Productos';
+    } else {
+      this.title = 'Título predeterminado para otras rutas';
+    }
+  }
+}
